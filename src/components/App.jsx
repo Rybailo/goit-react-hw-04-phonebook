78636664ec72cefc2fac5aec34930d9a982b6fde
Rybailo <rybailo.orest4@gmail.com>
2023-11-24 00:30:11 +0200

    add localstorge

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const stringifiedContacts = localStorage.getItem('contacts');
+    const parcedContacts =
+      JSON.parse(stringifiedContacts) ?? this.state.contacts;
+    return parcedContacts;
+  });
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const stringifiedContacts = JSON.stringify(contacts);
+    localStorage.setItem('contacts', stringifiedContacts);
+  }, [contacts]);
 
   const addContacts = contactData => {
     const hasDuplicates = contacts.some(
